c92b70795b91a92dd1adfbfcb7022e578c71180d
dilminx <pdminipura@gmail.com>
2026-02-12 17:10:45 +0530

    update cv

--- src/constants/index.jsx
+++ src/constants/index.jsx
@@ -27,7 +27,7 @@ export const NAVIGATION_LINKS = [
   { label: "Projects", href: "#projects" },
   { label: "Skills", href: "#skills" },
   { label: "Contact", href: "#contactform" },
-  { label: "Download Resume", href: "https://drive.google.com/file/d/1OarVdluGXFLHzX4BiRIq6tuxo09EcYtr/view?usp=sharing" },
+  { label: "Download Resume", href: "https://drive.google.com/file/d/12Sery1rr8sDVp0QvWxv7JEM4YFJauMNl/view?usp=sharing" },
 ];
 
 export const PROFILE = {
